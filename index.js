ac10db209300327e2b25cb221b7304e2a5e3d9a1
dxwc <dxwc@users.noreply.github.com>
2018-03-18 14:59:23 -0400

    fix repeated creation of the same event listener

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ let chat = new (require('events'))();
 
 let index = {}; index.html = path.join(__dirname, 'index.html');
 
+global.max_emit = 0;
+// TODO: use db/hashmap or something similar where closed
+// connections can be easily removed
+
+chat.on('someone-said-something', (by, said) =>
+{
+    console.log(global.max_emit);
+    for(let i = 1; i <= global.max_emit; ++i)
+    {
+        chat.emit(String(i), by, said);
+    }
+});
+
 app.get('/', (req, res) =>
 {
     if(typeof req.query.said === 'string' && typeof req.query.by === 'string')
@@ -23,9 +36,19 @@ app.get('/data', (req, res) =>
     res.header('Content-Type', 'text/event-stream');
     res.header('Cache-Control', 'no-cache');
 
-    chat.on('someone-said-something', (by, said) =>
+    let event_name = String(++global.max_emit);
+    chat.on(event_name, (by, said) =>
     {
         res.write(`data: ${JSON.stringify({ said: said, by : by })}\n\n`);
+    });
+
+    // TOFIX: event listener not being removed
+    res.on('finish', () =>
+    {
+        chat.removeListener(event_name, (by, said) =>
+        {
+            res.write(`data: ${JSON.stringify({ said: said, by : by })}\n\n`);
+        });
     });
 });
 
